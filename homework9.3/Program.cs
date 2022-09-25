d389caae1ce2703d5ee908e2aaf4874850b05899
YuliyaVas <yulenka2596@gmail.com>
2022-09-25 20:38:56 +0300

    Добавьте файлы проекта.

---
+++ homework9.3/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Task 9.3
+
+//    You are climbing a staircase. It takes n steps to reach the top.
+//Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+//Example 1:
+
+//Input: n = 2
+//Output: 2
+//Explanation: There are two ways to climb to the top.
+//1. 1 step + 1 step
+//2. 2 steps
+
+Console.WriteLine("Введите количество шагов");
+int n =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("количество способов: " + (ClimbStairs(n)));
+int ClimbStairs(int n) 
+{
+    if (n < 2)
+    {
+        return n;
+    }
+
+    int[] ans = new int[n];
+    ans[0] = 1;
+    ans[1] = 2;
+
+    for (int i = 2; i < n; i++) 
+    {
+        ans[i] = ans[i - 1] + ans[i - 2];
+    }
+    return ans[n - 1];
+}
+
+
+
---
+++ homework9/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Task 9.1
+//Given a positive integer n, return the smallest positive integer that is a multiple of both 2 and n.
+//    Constraints:
+//1 <= n <= 150
+
+Console.WriteLine("Введите положитльное число от 0 до 150");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int smallestEvenMultiple(int n)
+{
+        if ((1 <= n) && (n <= 150))
+        {
+            if ((n % 2 == 0))
+            { return n; }
+            else { return n * 2; }
+        }
+        else
+            Console.WriteLine("Число вне диапазона");
+        return 0;
+}
+    Console.WriteLine(smallestEvenMultiple(n)); 
+
